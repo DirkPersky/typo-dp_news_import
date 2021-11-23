0af78b7b58d650d0f2308035566c7341ba6dc501
Dirk.Persky <dirk.persky@gmail.com>
2021-11-23 13:17:18 +0100

    Start Development

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,32 +9,35 @@
  * @license    AGPL
  */
 
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Extbase\Object\Container\Container;
-use DirkPersky\NewsImport\Domain\Model\NewsDefault;
-
 defined('TYPO3_MODE') or die();
 
 $boot = function ($extKey) {
+    // New TYPO3 Version
+    if (class_exists(AbstractAdditionalFieldProvider::class) === true) {
+        $additionalFields = \DirkPersky\NewsImport\Tasks\ImportTaskAdditionalFieldProvider::class;
+    } else {
+        // TYPO3 8.7
+        $additionalFields = \DirkPersky\NewsImport\Tasks\ImportTaskAdditionalFieldProviderOld::class;
+    }
     // add Scheduler Task
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][\DirkPersky\NewsImport\Tasks\ImportTask::class] = [
         'extension' => $extKey,
         'title' => 'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang.xlf:task.name',
         'description' => 'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang.xlf:task.description',
-        'additionalFields' => \DirkPersky\NewsImport\Tasks\ImportTaskAdditionalFieldProvider::class
+        'additionalFields' => $additionalFields
     ];
     // register News Import Hool
-    GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\SignalSlot\\Dispatcher')->connect(
-        'GeorgRinger\\News\\Domain\\Service\\NewsImportService',
+    \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class)->connect(
+        \GeorgRinger\News\Domain\Service\NewsImportService::class,
         'postHydrate',
-        'DirkPersky\\NewsImport\\Aspect\\NewsImportAspect',
+        \DirkPersky\NewsImport\Aspect\NewsImportAspect::class,
         'postHydrate'
     );
 
     $GLOBALS['TYPO3_CONF_VARS']['EXT']['news']['classes']['Domain/Model/News'][] = $extKey;
 
-//    GeneralUtility::makeInstance(Container::class)
-//        ->registerImplementation(\GeorgRinger\News\Domain\Model\NewsDefault::class, NewsDefault::class);
+//    \TYPO3\CMS\Extbase\Object\Container\Container::makeInstance(Container::class)
+//        ->registerImplementation(\GeorgRinger\News\Domain\Model\NewsDefault::class, \DirkPersky\NewsImport\Domain\Model\NewsDefault::class);
 };
 
 $boot('dp_news_import');
